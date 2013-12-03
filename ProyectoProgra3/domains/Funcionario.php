c1b079ebf2cc0f5f105db5a05481a7b1371c7d89
propietario <javier13n@gmail.com>
2013-12-02 21:25:17 -0600

    02/12/2013 09:26 p.m.

---
+++ ProyectoProgra3/domains/Funcionario.php
@@ -0,0 +1,34 @@
+/******************************************************
+ * Autor: Javier Ramírez A
+ * Fecha: 02/12/2013
+ * Ultima modificación: 02/12/2013
+******************************************************/
+<?php
+
+	class Funcionario{
+		private $id;
+		private $idDepartamento;
+		private $nombre;
+		private $apellido1;
+		private $apellido2;
+		private $login;
+		private $contrasenna;
+		private $email;
+		private $estado;
+		
+		public function __construct(){
+			
+		}
+		
+		private function getId(){
+			return $id;
+		}
+		
+		private function setId($id){
+			$this->id = $id;
+		}
+		
+		
+	}
+
+?>
